8ae14be643232347547df3a39d28dcdb9241d76e
Ashikur Rahman <ashikur.jebu111@gmail.com>
2026-02-08 23:42:18 +0600

    docker

---
+++ NOTES.md
@@ -0,0 +1,21 @@
+# Notes
+
+- HTTP is stateless and unidirectional.
+- No way to push data from server to client unless(setInterval from client)
+- WebSockets provide a persistent, bidirectional connection
+- Both server and client can send data at any time
+- Socket.io is a JavaScript library that abstracts WebSockets.
+- It supports fallbacks for unsupported browsers.
+- Support creation of rooms, namespaces, broadcasting.
+
+## What Will We Learn Today?
+
+- What is Containerization?
+- What is Docker?
+- Installing Docker
+- Dockerizing the Backend (Express + Socket.IO)
+- Dockerizing the Frontend (React + Vite)
+- NginX
+- Docker Compose to Tie It All Together
+- How to Run the Whole Stack Locally?
+- Wrap-Up
---
+++ ui/src/components/ChatRoom.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState, useRef } from "react";
+import io from "socket.io-client";
+
+import PropTypes from "prop-types";
+
+const socket = io.connect("http://localhost:3001");
+
+function ChatRoom({ username, room }) {
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [typingMsg, setTypingMsg] = useState("");
+
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        socket.emit("join_room", room);
+
+        socket.on("receive_message", (data) => {
+            setMessages((prev) => [...prev, data]);
+        });
+
+        socket.on("user_typing", (user) => {
+            setTypingMsg(`${user} is typing...`);
+            setTimeout(() => setTypingMsg(""), 2000);
+        });
+
+        return () => {
+            socket.off("receive_message");
+            socket.off("user_typing");
+        };
+    }, [room]);
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages])
+
+    const sendMessage = () => {
+        if (message.trim()) {
+            const messageData = {
+                room,
+                author: username,
+                message,
+                id: crypto.randomUUID(),
+                time: new Date().toLocaleTimeString()
+            }
+            socket.emit('send_message', messageData);
+            setMessages((prev) => [...prev, messageData]);
+            setMessage('');
+        }
+    };
+
+    const handleTyping = () => {
+        socket.emit('typing', {username, room});
+    }
+
+    return (
+        <div className="w-full max-w-2xl bg-white p-4 rounded shadow">
+            <h2 className="text-xl font-bold mb-2">
+                Room: {room}({username})
+            </h2>
+            <div className="h-64 overflow-y-auto border p-2 mb-2 bg-gray-50 rounded">
+                {messages.map((msg) => (
+                    <div key={msg.id}>
+                        <span className="font-semibold">{msg.author}: </span>{" "}
+                        {msg.message}
+                        <div className="text-xs text-gray-400">{msg.time}</div>
+                    </div>
+                ))}
+                <div ref={messagesEndRef} />
+            </div>
+
+            <p className="text-sm italic text-gray-500 mb-2">{typingMsg}</p>
+
+            <input
+                type="text"
+                value={message}
+                placeholder="Type a message..."
+                onChange={(e) => {
+                    setMessage(e.target.value);
+                    handleTyping();
+                }}
+                onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+                className="flex-1 p-2 border rounded"
+            />
+        </div>
+    );
+}
+
+ChatRoom.propTypes = {
+    username: PropTypes.string.isRequired,
+    room: PropTypes.string.isRequired,
+};
+
+export default ChatRoom;
